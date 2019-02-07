ced2967e4938898c0da8f5ed7ef41814333d5fd7
Frederick <westbrookfrederick@gmail.com>
2019-02-06 21:29:23 -0600

    initial

---
+++ Script/about.js
@@ -0,0 +1,33 @@
+//About Page About Us Slide
+let mission = document.getElementById("missionBtn");
+let vision = document.getElementById("visionBtn");
+let situation = document.getElementById("situationBtn");
+
+//Event Listeners
+mission.addEventListener("click", contentLink);
+vision.addEventListener("click", contentLink);
+situation.addEventListener("click", contentLink);
+
+//Function
+function contentLink() {
+  var allContent = document.querySelectorAll(".valuesContent section");
+  for (var i = 0; i < allContent.length; i++) {
+    allContent[i].className = "hidden";
+  }
+  var contentId = this.attributes["data-type"].value;
+  var content = document.getElementById(contentId);
+  if (content.className === "hidden") {
+    content.className = "";
+  } else {
+    content.className = "hide";
+  }
+}
+//Open Consultation Form
+let conOpened = document.getElementById("consultBtn");
+
+conOpened.addEventListener("click", openconsultForm);
+
+function openconsultForm() {
+  const conMod = document.getElementById("conModule");
+  conMod.style.display = "block";
+}
